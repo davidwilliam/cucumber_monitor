15fb74b0829029aeaf54f68087d154a5d6693510
David William <david@webhall.com.br>
2012-09-23 13:21:00 -0300

    Adds default path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resource :cucumber_monitor, only: [] do 
+  	get '/' => 'cucumber_monitor#features', as: 'features'
     get 'features' => 'cucumber_monitor#features', as: 'features'
     get 'show_feature/:name' => 'cucumber_monitor#show_feature', as: 'show_feature'
     get 'search' => 'cucumber_monitor#search', as: 'search'
